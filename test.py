7a0c15a1f3691da8d9fe48da08df23d87bc8a0a0
Ankit <ankitgarg1@gmail.com>
2019-05-22 17:30:21 +0530

    test case for 10 million nodes

--- test.py
+++ test.py
@@ -1,13 +1,31 @@
 from GetShortestPath import User, getPath
+import time
 
 userArr = []
 lastUser = None
-for i in range(10000):
+
+val = 10000000
+
+for i in range(100000):
     name = "Ankit" + str(i)
     user = User(name, i)
+    for k in range(100):
+        new_name = "Friend" + "-" + str(i) + "-" + str(k)
+        new_user = User(new_name, val)
+        val += 1
+        user.addFriend(new_user)
+
     if lastUser is not None:
         lastUser.addFriend(user)
+        
     userArr.append(user)
     lastUser = user
 
-print(getPath(userArr[0], userArr[10]))
+print("graph plotted")
+time_start = int(time.time() * 1000)
+getPath(userArr[0], userArr[10000])
+# print(getPath(userArr[0], userArr[10000]))
+time_end = int(time.time() * 1000)
+print("graph plotted", time_end-time_start)
+
+
